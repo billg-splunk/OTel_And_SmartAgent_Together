7742ce6ee48a1c4f4f2601a5a691573d0e6f4435
Bill Grant <billg@splunk.com>
2021-10-20 10:27:09 -0400

    quotes

--- deployOTel.sh
+++ deployOTel.sh
@@ -9,4 +9,4 @@ TOKEN=$1
 
 helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
 helm repo update
-helm install -f values-otel.yaml --set provider=' ' --set distro=' ' --set splunkObservability.accessToken='$TOKEN' --set clusterName='MyClusterOTel' --set splunkObservability.realm='us1' --set otelCollector.enabled='false'  --set splunkObservability.logsEnabled='false'  --generate-name splunk-otel-collector-chart/splunk-otel-collector
+helm install -f values-otel.yaml --set provider=' ' --set distro=' ' --set splunkObservability.accessToken=$TOKEN --set clusterName='MyClusterOTel' --set splunkObservability.realm='us1' --set otelCollector.enabled='false'  --set splunkObservability.logsEnabled='false'  --generate-name splunk-otel-collector-chart/splunk-otel-collector
