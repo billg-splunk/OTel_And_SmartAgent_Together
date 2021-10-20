443201c0420049eb0e2e6f2b010e20862696975f
Bill Grant <billg@splunk.com>
2021-10-19 21:59:38 -0400

    fixes

--- apps/dockerbuilds.sh
+++ apps/dockerbuilds.sh
@@ -1,2 +1,2 @@
-sudo docker build . -f apps/app-otel.dockerfile -t app-otel
-sudo docker build . -f apps/app-sa.dockerfile -t app-sa
+sudo docker build . -f app-otel.dockerfile -t app-otel
+sudo docker build . -f app-smargagent.dockerfile -t app-sa
