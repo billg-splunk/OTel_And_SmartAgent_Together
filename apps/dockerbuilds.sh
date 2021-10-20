2692ca6ebcc34cfec5f644318ceb33fbce1187b8
Bill Grant <billg@splunk.com>
2021-10-19 23:52:51 -0400

    Updates

--- apps/dockerbuilds.sh
+++ apps/dockerbuilds.sh
@@ -9,3 +9,8 @@ microk8s ctr image import app-otel.tar
 microk8s ctr image import app-sa.tar
 rm app-otel.tar
 rm app-sa.tar
+
+
+helm upgrade -f values-otel-harnit.yaml --set provider=' ' --set distro=' ' --set splunkObservability.accessToken=‘ijZdEC_FU3lEhJdUiV330g’ --set clusterName='MyCluster' --set splunkObservability.realm='us1' --set otelCollector.enabled='false'  --set splunkObservability.logsEnabled='false' splunk-otel-collector-1633729006 splunk-otel-collector-chart/splunk-otel-collector
+
+helm upgrade -f values-otel-harnit.yaml --set provider=' ' --set distro=' ' --set splunkObservability.accessToken='ijZdEC_FU3lEhJdUiV330g' --set clusterName='MyCluster' --set splunkObservability.realm='us1' --set otelCollector.enabled='false'  --set splunkObservability.logsEnabled='false' splunk-otel-collector-1633729006 splunk-otel-collector-chart/splunk-otel-collector
